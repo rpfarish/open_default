fce0d0a9fe0970294989016c2ec5ce3b123a754e
Ryan <rpfarish@live.com>
2020-08-10 17:48:45 -0400

    call sites from main and added args for help

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 import webbrowser
 
-nums = [1, 2, 4, 3, 7]
-data = [4, 4, 4, 4, 4, 4, 7]
 source = "sites.txt"
 
 
@@ -37,6 +35,13 @@ class Dbtype:
         else:
             return -1
 
+    @classmethod
+    def get_all_sites(cls):
+        for obj in cls.types:
+            print(obj.name)
+            print(obj.desc)
+            print(obj.sites)
+
     def __repr__(self):
         return f'Type: {self.name}, Description: {self.desc} \nSites: {self.sites}'
 
@@ -55,7 +60,6 @@ class FileParseError(Exception):
         return f"{self.message} on line {self.line_num}: '{self.line}'"
 
 
-
 def setup(file):
     type_obj = []
 
@@ -68,8 +72,6 @@ def setup(file):
                 link = []
                 title = line[0:-2]
 
-                # print('thats not valid')
-
             elif '}' in line:
                 type_obj.append(Dbtype(title, about, link))
             elif ',' in line:
@@ -78,12 +80,11 @@ def setup(file):
                 about.append(line[0])
                 link.append(line[1])
             else:
-                raise FileParseError(line, line_num+1)
-
-        # 'FileError: Input file not correctly formatted')
+                raise FileParseError(line, line_num + 1)
 
     # for i in type_obj:
     #     print(i)
+    # Dbtype.get_all_sites()
     return type_obj
 
 
@@ -162,7 +163,10 @@ def call_error(arg, msg=None, pre_msg=None):
 def parse_type_call(args):
     # chck if arg for type is valid (from DBtype class)
     if len(args) >= 4:
-        if args[0:4] == 'type':
+        if args == 'type':
+            return True
+
+        if args.startswith('type'):
             args = args.split(' ')
             return args[1:]
 
@@ -172,10 +176,23 @@ def parse_help_call(args):
     if args == 'help':
         return 'help'
     if len(args) >= 4:
-        if args[0:4] == 'help':
+        if args.startswith('help'):
             args = args.split(' ')
             args = args[1:]
             return args
+
+
+def foo(objs):
+    num = 0
+    rad = []
+    for n, i in enumerate(objs):
+        # print("Type:", i.name)
+        for _, s in enumerate(zip(i.desc, i.sites)):
+            rad.append(s[1])
+            # print('\t', num, ' , '.join(s))
+            num += 1
+    return rad
+    # print('we are', rad, 'number 1')
 
 
 # what functions do i need?
@@ -207,21 +224,39 @@ def main_loop(objs):
         if arg == 'restart':
             print('-' * 20, 'RESTART', '-' * 20)
         if type_call := parse_help_call(arg):
-            if type_call[0] == 'type':
+            if 'all' in type_call:
+                print('opens all of the specified type')
+                continue
+            elif 'copyright' in type_call:
+                print('Prints the copyright')
+                continue
+            elif 'help' in type_call and type(type_call) == list:
+                print("Calling help 'thing' prints help for the object 'thing'.")
+                continue
+            elif 'restart' in type_call:
+                print("Prints a long line.")
+                continue
+            elif 'quit' in type_call:
+                print("Exits the program.")
+                continue
+            elif 'type' in type_call:
                 print('type in name of the catagory you want to switch to.')
                 print("If you want to go back to no type enter 'type none'")
                 continue
+
             if curr_type is None:
+                num = 0
                 for n, i in enumerate(objs):
                     print("Type:", i.name)
-                    for num, s in enumerate(zip(i.desc, i.sites)):
-                        print('\t', num, ' , '.join(s))
+                    for _, s in enumerate(zip(i.desc, i.sites)):
+                        print('\t', num, ', '.join(s))
+                        num += 1
             else:
                 for n, i in enumerate(objs):
                     if i.name == curr_type:
                         print("Type:", i.name)
                         for num, s in enumerate(zip(i.desc, i.sites)):
-                            print('\t', num, ' , '.join(s))
+                            print('\t', num, ', '.join(s))
             print('all\t\t       Opens all links')
             print('copyright\t   Prints the copyright')
             print('help\t\t   Gives info based on current context')
@@ -238,6 +273,9 @@ def main_loop(objs):
         elif type_call := parse_type_call(arg):
             # validate
 
+            if type_call is True:
+                print('Builtin type function\ntype "help type" for more')
+                continue
             curr_type = type_call[0]
             if curr_type == 'none':
                 curr_type = None
@@ -254,12 +292,15 @@ def main_loop(objs):
             quit()
         # can use code as a kwarg for quit to have a exit code and a red error msg
 
-        elif curr_type is None:
-            call_error(arg, ' was not given in the correct syntax or the correct range ', "The command ")
+        elif curr_type is None and (open_sites := validate_data(arg, (site_li := foo(objs)))):
+            if open_sites == -1:
+                call_error(arg, ' was not given in the correct syntax or the correct range ', "The command ")
+            else:
+                call_some(open_sites, site_li)
             continue
         # clean and check data
         elif open_sites := validate_data(arg, Dbtype.get_sites_by_name(curr_type)):
-
+            print(open_sites, Dbtype.get_sites_by_name(curr_type))
             if open_sites == -1:
                 call_error(arg, ' was not given in the correct syntax or the correct range ', "The command ")
 
@@ -273,7 +314,8 @@ def main_loop(objs):
 
 
 if __name__ == '__main__':
-    print('\nOpen Default 1.5.2 (tags/v2.1:6f8c832, May 13 2020, 22:20:19)')
+    print('Open Default 1.5.2 (tags/v2.1:6f8c832, Jul 27 2020, 20:27:19)')
     print('Type "help" or "copyright" for more information.')
     obj = setup(source)
+    # print(obj)
     main_loop(obj)
